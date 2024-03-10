a971b7af1d58fa0d7cbd0f014c7039bfcbd57964
MarioPasc <mario.pg02@gmail.com>
2024-03-10 16:52:01 +0100

    Labels ROI YOLOv8 hecho

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,13 +2,25 @@ import os
 import numpy as np
 import cv2 as cv
 import nibabel as nib
+from sklearn.model_selection import train_test_split
 
 
-def convert_gz_nii(niigz_file, save_path, patient_id, exclude_slices):
+# Esta función convierte un estudio nii.gz a diversas imágenes .nii, asociando como nombre
+# de las imágenes idpaciente-slice.nii.
+# VER2: Se añade también un parámetro de entrada que es una lista de rodajas que no se deben incluir
+def convert_gz_nii(niigz_file, save_path, path_excluir, patient_id):
     # Verificar si el archivo .nii.gz existe
     if not os.path.isfile(niigz_file):
         print("Error: Specified .nii.gz file doesn't exist.")
         return
+    # Creamos el vector con las rodajas a exlcuir
+    exclude_slices = []
+    # Si existe el archivo .txt, rellenamos el vector con las rodajas que hay que excluir
+    # Si no existe, se asume que se incluyen todas.
+    if os.path.isfile(path_excluir):
+        with open(path_excluir, 'r') as archivo:
+            for linea in archivo:
+                exclude_slices.append(int(linea.strip()))
 
     # Cargar el archivo .nii.gz
     img = nib.load(niigz_file)
@@ -36,7 +48,8 @@ def convert_gz_nii(niigz_file, save_path, patient_id, exclude_slices):
     print("Process finished with exit code 0")
 
 
-def study_to_nii(study_path, save_path, exclude_slices):
+# Esta función hace uso de la anterior para convertir un estudio completo a formato .nii
+def study_to_nii(study_path, save_path, path_excluir):
     if not os.path.exists(study_path):
         print("Input file does not exist")
         return
@@ -57,46 +70,149 @@ def study_to_nii(study_path, save_path, exclude_slices):
 
         # Asumimos que solo hay un archivo .nii.gz por paciente
         niigz_file = os.path.join(patient_folder, niigz_files[0])
-        convert_gz_nii(niigz_file=niigz_file, save_path=save_path, patient_id=patient_id, exclude_slices=exclude_slices)
+        convert_gz_nii(niigz_file=niigz_file, save_path=save_path, patient_id=patient_id, path_excluir=path_excluir)
 
 
-def convert_nii_to_image(input_folder, output_folder, imformat):
-    # Verificar si el directorio de entrada existe
-    if not os.path.isdir(input_folder):
-        print("Input folder does not exist.")
+# Esta función toma como entrada una carpeta con imágenes .nii y las guarda en la carpeta de destino
+# en el formato especificado.
+def convert_nii_image_holdout(input_txt):
+    # Verificar si el archivo .txt existe
+    if not os.path.isfile(input_txt):
+        print("Input txt file does not exist.")
         return
-    # Crear el directorio de salida si no existe
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
 
-    # Obtener la lista de archivos .nii en el directorio de entrada
-    nii_files = [file for file in os.listdir(input_folder) if file.endswith('.nii')]
+    # Leer el archivo .txt
+    with open(input_txt, 'r') as file:
+        lines = file.readlines()
 
-    for nii_file in nii_files:
-        # Cargar el archivo .nii
-        img = nib.load(os.path.join(input_folder, nii_file))
-        data = img.get_fdata()
+    # Obtener las rutas de los folders y archivos
+    nii_folder = lines[0].split(':')[1].strip()
+    train_folder = lines[1].split(':')[1].strip()
+    train_nii_files = lines[2].split(':')[1].strip()
+    val_folder = lines[3].split(':')[1].strip()
+    val_nii_files = lines[4].split(':')[1].strip()
+    test_folder = lines[5].split(':')[1].strip()
+    test_nii_files = lines[6].split(':')[1].strip()
+    imformat = lines[7].split(':')[1].strip().lower()
 
-        # Verificar si hay valores no válidos en los datos
-        if np.any(np.isnan(data)) or np.any(np.isinf(data)):
-            print(f"Skipping {nii_file}: Invalid values encountered.")
-            continue
+    # Función para convertir .nii a imagen
+    def convert_to_image(nii_files, folder):
+        for nii_file in nii_files:
+            # Cargar el archivo .nii
+            img = nib.load(os.path.join(nii_folder, nii_file))
+            data = img.get_fdata()
 
-        # Verificar si el rango de los datos es válido
-        min_val = np.min(data)
-        max_val = np.max(data)
-        if min_val == max_val:
-            print(f"Skipping {nii_file}: Invalid data range.")
-            continue
+            # Verificar si hay valores no válidos en los datos
+            if np.any(np.isnan(data)) or np.any(np.isinf(data)):
+                print(f"Skipping {nii_file}: Invalid values encountered.")
+                continue
 
-        # Normalizar los valores de píxel para que estén entre 0 y 255
-        data_normalized = ((data - min_val) / (max_val - min_val) * 255).astype(np.uint8)
-        # Obtener el nombre del archivo sin la extensión .nii
-        file_name, _ = os.path.splitext(nii_file)
-        # Crear el nombre de la imagen con el formato especificado
-        image_name = f"{file_name}.{imformat.lower()}"
-        # Guardar la imagen en el formato especificado
-        cv.imwrite(os.path.join(output_folder, image_name), data_normalized)
+            # ========== NORMALIZACIÓN PROVISIONAL ==========
+            # Normalizar los valores de píxel para que estén entre 0 y 255
+            data_normalized = ((data - np.min(data)) / (np.max(data) - np.min(data)) * 255).astype(np.uint8)
+            # ========== FIN NORMALIZACIÓN PROVISIONAL ======
+
+            # Obtener el nombre del archivo sin la extensión .nii
+            file_name, _ = os.path.splitext(nii_file)
+            # Crear el nombre de la imagen con el formato especificado
+            image_name = f"{file_name}.{imformat}"
+            # Guardar la imagen en el formato especificado
+            cv.imwrite(os.path.join(folder, image_name), data_normalized)
+
+    # Obtener los nombres de archivos .nii para train, val y test
+    train_nii_files = [file.strip() for file in open(train_nii_files, 'r').readlines()]
+    val_nii_files = [file.strip() for file in open(val_nii_files, 'r').readlines()]
+    test_nii_files = [file.strip() for file in open(test_nii_files, 'r').readlines()]
+
+    # Convertir .nii a imágenes para train, val y test
+    convert_to_image(train_nii_files, train_folder)
+    convert_to_image(val_nii_files, val_folder)
+    convert_to_image(test_nii_files, test_folder)
+
+    print("Process finished with exit code 0.")
+
+
+# Esta función se encarga del hold out (gracias a dios que existe scikit-learn)
+def holdout_nii_images(folder_path, val_percent, test_percent, output_path):
+    # Obtener la lista de archivos .nii en la carpeta
+    nii_files = [file for file in os.listdir(folder_path) if file.endswith('.nii')]
+
+    # Dividir los nombres de los archivos en train, val y test
+    train_files, val_test_files = train_test_split(nii_files, test_size=(val_percent + test_percent), random_state=42)
+    val_files, test_files = train_test_split(val_test_files, test_size=test_percent / (val_percent + test_percent),
+                                             random_state=42)
+
+    # Escribir los nombres de los archivos en archivos de texto
+    def write_to_txt(file_list, txt_path):
+        with open(txt_path, 'w') as file:
+            for file_name in file_list:
+                file.write(file_name + '\n')
+
+    write_to_txt(train_files, os.path.join(output_path, 'train_files.txt'))
+    write_to_txt(val_files, os.path.join(output_path, 'val_files.txt'))
+    write_to_txt(test_files, os.path.join(output_path, 'test_files.txt'))
+
+    print("Hold-out completed successfully.")
+
+
+def contours_YOLO_format(contours, height, width, output_path):
+    if len(contours) > 0:
+        with open(output_path, 'w') as f:
+            for i, contorno in enumerate(contours):
+                normalized = contorno / np.array([width, height])
+                str_contour = ' '.join([f"{coord:.3f}" for coord in normalized.flatten()])
+                str_contour = "0 " + str_contour
+                f.write(f"{str_contour}\n")
+    else:
+        return
+
+
+def extract_roi_contours(input_txt):
+    # Verificar si el archivo .txt existe
+    if not os.path.isfile(input_txt):
+        print("Input txt file does not exist.")
+        return
+
+    # Leer el archivo .txt
+    with open(input_txt, 'r') as file:
+        lines = file.readlines()
+
+    # Obtener las rutas de los folders y archivos
+    nii_folder = lines[0].split(':')[1].strip()
+    train_folder = lines[1].split(':')[1].strip()
+    train_nii_files = lines[2].split(':')[1].strip()
+    val_folder = lines[3].split(':')[1].strip()
+    val_nii_files = lines[4].split(':')[1].strip()
+    test_folder = lines[5].split(':')[1].strip()
+    test_nii_files = lines[6].split(':')[1].strip()
+
+    # Función para extraer y guardar contornos
+    def save_contours(nii_files, folder):
+        for nii_file in nii_files:
+            # Cargar el archivo .nii
+            img = nib.load(os.path.join(nii_folder, nii_file))
+            data = img.get_fdata()
+
+            # Obtener los contornos utilizando cv.findContours
+            contours, _ = cv.findContours(data.astype(np.uint8), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+            # Obtener las dimensiones de la imagen
+            width = data.shape[0]
+            height = data.shape[1]
+
+            # Guardar los contornos en formato YOLO
+            output_path = os.path.join(folder, f"contours_{nii_file.strip('.nii')}.txt")
+            contours_YOLO_format(contours=contours, height=height, width=width, output_path=output_path)
+
+    # Obtener los nombres de archivos .nii para train, val y test
+    train_nii_files = [file.strip() for file in open(train_nii_files, 'r').readlines()]
+    val_nii_files = [file.strip() for file in open(val_nii_files, 'r').readlines()]
+    test_nii_files = [file.strip() for file in open(test_nii_files, 'r').readlines()]
+
+    # Extraer y guardar contornos para train, val y test
+    save_contours(train_nii_files, train_folder)
+    save_contours(val_nii_files, val_folder)
+    save_contours(test_nii_files, test_folder)
 
     print("Process finished with exit code 0.")
 
