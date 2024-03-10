85900d09a49c16dd60a39f5732b4a434770433b1
MarioPasc <mario.pg02@gmail.com>
2024-03-10 20:15:08 +0100

    Main llamable desde bash

--- src/yolov8_train.py
+++ src/yolov8_train.py
@@ -1,12 +1,15 @@
-def main():
-    from ultralytics import YOLO
+import argparse
+from ultralytics import YOLO
 
+def main(yaml_path):
     # Load a model
     model = YOLO('yolov8n-seg.pt')  # load a pretrained model (recommended for training)
 
     # Train the model
-    path_yaml = "/home/mariopasc/Python/Projects/BSC_final/epilepsy-displasia-focal-segmentation/text-info-files/t2flair-study/config.yaml"
-    results = model.train(data=path_yaml, epochs=100, imgsz=256)
+    results = model.train(data=yaml_path, epochs=100, imgsz=256)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Train YOLO model.')
+    parser.add_argument('yaml_path', type=str, help='Path to YAML file')
+    args = parser.parse_args()
+    main(args.yaml_path)
