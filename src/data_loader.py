2ebbc7e00cf4295690339a514afa5d4728afbeb7
MarioPasc <mario.pg02@gmail.com>
2024-04-09 20:31:40 +0200

    No augmentation for test- refac

--- src/data_loader.py
+++ src/data_loader.py
@@ -43,7 +43,6 @@ class DataLoader:
             for folder in ["T2FLAIR", "T1WEIGHTED", "ROI_T2", "ROI_T1"]:
                 study_folder = os.path.join(base_path, folder)
                 os.makedirs(study_folder, exist_ok=True)
-                os.makedirs(os.path.join(study_folder, "TEST"), exist_ok=True)
 
         for patient in self.patients_roi:
             anat_path = os.path.join(self.dataset_path, patient, 'anat')
@@ -68,12 +67,29 @@ class HoldOut:
 
         self.roi_study = roi_study
         self.study_name = study_name
+        self.dataset_path = dataset_path
+
         self.val_percent = val_percent
         self.test_percent = test_percent
-        self.dataset_path = dataset_path
         self.__holdoutNiigz__()
-        self.__relocateTestFiles__()
-        
+
+    def organizeAugmentation(self) -> None:  
+        study_path = os.path.join(self.dataset_path, self.study_name)
+        patients_train = [patient.split("_")[0] + "*" for patient in self.train_set]
+        train_all = list(chain(*[glob.glob(os.path.join(study_path, patientAug)) for patientAug in patients_train]))
+        self.train_set = [os.path.basename(item) for item in train_all]
+
+        patients_val = [patient.split("_")[0] + "*" for patient in self.val_set]
+        val_all = list(chain(*[glob.glob(os.path.join(study_path, patientAug)) for patientAug in patients_val]))
+        self.val_set = [os.path.basename(item) for item in val_all]
+
+
+    def __contoursYOLO__(self, contours: List[List[int]], height: int, width: int, output_path: str) -> None:
+        with open(output_path, 'w') as f:
+            for contour in contours:
+                normalized = contour.squeeze().astype('float') / np.array([width, height])
+                str_contour = ' '.join([f"{coord:.6f}" for coord in normalized.flatten()])
+                f.write(f"0 {str_contour}\n")    
 
     def __holdoutNiigz__(self) -> None:
         self.train_set.clear()
@@ -100,146 +116,25 @@ class HoldOut:
             print(f"Hold-out error. The total file amount is {len(niigz_files)}" +
                   "and the sum of all sets is {len(self.train_set) + len(self.val_set) + len(self.test_set)}.")
 
-    def __relocateTestFiles__(self) -> None:
+    """
+    # We want to relocate the files in train test val folders before generating the final dataset
+    def __relocateFiles__(self):
         study_path = os.path.join(self.dataset_path, self.study_name)
+        for setFolder in ["train", "val", "test"]:
+            os.makedirs(os.path.join(study_path, setFolder), exist_ok=True)
+
         for file in os.listdir(study_path):
-            if file in self.test_set:
-                shutil.move(src=os.path.join(study_path, file),
-                            dst=os.path.join(study_path, "TEST"))
+            niigz_filePath = os.path.join(study_path, file)
+            if file in self.train_set:
+                shutil.move(niigz_filePath, os.path.join(study_path, "train"))
+            elif file in self.val_set:
+                shutil.move(niigz_filePath, os.path.join(study_path, "val"))
+            elif file in self.test_set:
+                shutil.move(niigz_filePath, os.path.join(study_path, "test")) """
 
-    def __niiPng__(self, nii_path: str, output_path: str) -> None:
-        try:
-            nii_volume = nib.load(nii_path)
-            nii_data = nii_volume.get_fdata()
-        except Exception as e:
-            print(f"Failed to load {nii_path}: {e}")
-            return
-        
-        # Extract patient ID and augmentation type from the file name
-        file_name = os.path.basename(nii_path)
-        id_patient, augmentation_type = file_name.split('_')[0], file_name.split('_')[-2]
-        # Determine the output file name format based on the augmentation type
-        if augmentation_type in ["gamma", "brightness", "flip", "shift"]:
-            id_augmentation = file_name.split('.')[0].split('_')[-1]
-            output_format = f"{id_patient}_slice-{{i}}_{augmentation_type}_{id_augmentation}.png"
-        else:
-            output_format = f"{id_patient}_slice-{{i}}.png"
-
-        # Convert each slice to a .png file
-        exclude = list(range(0, 121)) + list(range(200, 257))
-        for i in range(nii_data.shape[2]):
-            if output_format == f"{id_patient}_slice-{{i}}.png":
-                # Exclude only images that have more than a hundred images per patient
-                if i in exclude and nii_data.shape[2] > 100:
-                    continue    
-            slice_data = nii_data[:, :, i]
-            # Normalize the slice data to the range [0, 255]
-            slice_normalized = cv2.normalize(slice_data, None, 0, 255, cv2.NORM_MINMAX)
-            slice_uint8 = np.uint8(slice_normalized)
-            # Construct the output file name
-            output_file_name = output_format.format(i=i)
-            if id_patient == "sub-00001": print(f"Image output: {output_file_name}")
-
-            output_file_path = os.path.join(output_path, output_file_name)
-            # Save the slice as a .png file
-            cv2.imwrite(output_file_path, slice_uint8)        
+    
+    
+    
             
-                
-    def __roiContours__(self, nii_path: str, output_path: str) -> None:
-        roi_path = os.path.join(self.dataset_path, self.roi_study)
-        # Determine the output file name format based on the augmentation type
-        # Extract patient ID and augmentation type from the file name
-        file_name = os.path.basename(nii_path)
-        id_patient, augmentation_type = file_name.split('_')[0], file_name.split('_')[-2]
-        # Determine the output file name format based on the augmentation type
-        if augmentation_type in ["gamma", "brightness", "flip", "shift"]:
-            id_augmentation = file_name.split('.')[0].split('_')[-1]
-            output_format = f"{id_patient}_slice-{{i}}_{augmentation_type}_{id_augmentation}.txt"
-            matching_files = glob.glob(os.path.join(roi_path, f"{id_patient}*{augmentation_type}_{id_augmentation}.nii.gz"))
-        else:
-            output_format = f"{id_patient}_slice-{{i}}.txt"    
-            matching_files = glob.glob(os.path.join(roi_path, f"{id_patient}*.nii.gz"))
-
-        
-        if not matching_files:
-            print(f"No ROI files found for patient ID: {id_patient}")
-            return # Skip this patient if no matching files are found
-
-        file = os.path.join(roi_path, matching_files[0])    
-        roi_niigz = nib.load(os.path.join(roi_path, file)).get_fdata()
-        exclude = list(range(0, 121)) + list(range(200, 257))
-        for i in range(roi_niigz.shape[2]):
-            if output_format == f"{id_patient}_slice-{{i}}.txt":
-                # Exclude only images that have more than a hundred images per patient
-                if i in exclude and roi_niigz.shape[2] > 100:
-                    continue
-
-            slice = roi_niigz[:, :, i].astype(np.uint8)
-            contours, _ = cv2.findContours(slice, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            # Extract the contours that have at least 3 points : [[x1, y1], [x2, y2], [x3, y3], ...]
-            contours = [contour for contour in contours if len(contour) >= 3]
-            if contours:
-                # Construct the output file name
-                output_file_name = output_format.format(i=i)
-                if id_patient == "sub-00001": print(f"Image output: {output_file_name}")
-
-                output_file_path = os.path.join(output_path, output_file_name)
-                self.__contoursYOLO__(contours=contours,
-                                      height=slice.shape[0],
-                                      width=slice.shape[1],
-                                      output_path=output_file_path)
-
-    def __contoursYOLO__(self, contours: List[List[int]], height: int, width: int, output_path: str) -> None:
-        with open(output_path, 'w') as f:
-            for contour in contours:
-                normalized = contour.squeeze().astype('float') / np.array([width, height])
-                str_contour = ' '.join([f"{coord:.6f}" for coord in normalized.flatten()])
-                f.write(f"0 {str_contour}\n")
-
-    def organizeAugmentation(self) -> None:  
-        study_path = os.path.join(self.dataset_path, self.study_name)
-        patients_train = [patient.split("_")[0] + "*" for patient in self.train_set]
-        train_all = list(chain(*[glob.glob(os.path.join(study_path, patientAug)) for patientAug in patients_train]))
-        self.train_set = [os.path.basename(item) for item in train_all]
-
-        patients_val = [patient.split("_")[0] + "*" for patient in self.val_set]
-        val_all = list(chain(*[glob.glob(os.path.join(study_path, patientAug)) for patientAug in patients_val]))
-        self.val_set = [os.path.basename(item) for item in val_all]
-
-        
-
-    def holdout(self) -> None:
-        # Create folder structure
-        base_path = os.path.join(self.dataset_path, "..", f"{self.study_name}-ds-epilepsy")
-        if (not os.path.exists(base_path)):
-            os.makedirs(base_path, exist_ok=True)
-            for data_type in ["images", "labels"]:
-                for folder in ["train", "val", "test"]:
-                    os.makedirs(os.path.join(base_path, data_type, folder), exist_ok=True)
-        # 1. Copy niigz files as png in their respective holdout folder
-        # 2. Extract ROI contours and save them in their respective holdout folder
-        study_path = os.path.join(self.dataset_path, self.study_name)
-        nifti_files = os.listdir(study_path)
-        for niigz_file in tqdm(nifti_files, desc="Generating YOLO dataset"):
-            nii_path = os.path.join(study_path, niigz_file)
-            
-            if niigz_file in self.train_set:
-                image_output_path = os.path.join(base_path, "images", "train")
-                label_output_path = os.path.join(base_path, "labels", "train")
-            elif niigz_file in self.val_set:
-                image_output_path = os.path.join(base_path, "images", "val")
-                label_output_path = os.path.join(base_path, "labels", "val")
-            else:
-                continue
-
-            self.__niiPng__(nii_path=nii_path, output_path=image_output_path)
-            self.__roiContours__(nii_path=nii_path, output_path=label_output_path)
-
-        for test_image in os.listdir(os.path.join(study_path, "TEST")):
-            image_output_path = os.path.join(base_path, "images", "test")
-            label_output_path = os.path.join(base_path, "labels", "test")
-            nii_path = os.path.join(study_path, "TEST", test_image)
-            self.__niiPng__(nii_path=nii_path, output_path=image_output_path)
-            self.__roiContours__(nii_path=nii_path, output_path=label_output_path)
 
 
--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -4,14 +4,17 @@ import data_explore
 
 def main():
     dl_instance = data_loader.DataLoader(dataset_path="/home/mariopasc/Python/Datasets/epilepsy")
-    dataaug_instance = data_augmentation.DataAugmentation(study_path="/home/mariopasc/Python/Datasets/ds-epilepsy/T2FLAIR",
-                                        roi_path="/home/mariopasc/Python/Datasets/ds-epilepsy/ROI_T2")
     holdout_instance = data_loader.HoldOut(dataset_path="/home/mariopasc/Python/Datasets/ds-epilepsy",
                                study_name="T2FLAIR", roi_study="ROI_T2", val_percent=.2, test_percent=.2)
+    print(f"Test: {holdout_instance.test_set}")
+    dataaug_instance = data_augmentation.DataAugmentation(study_path="/home/mariopasc/Python/Datasets/ds-epilepsy/T2FLAIR",
+                                        roi_path="/home/mariopasc/Python/Datasets/ds-epilepsy/ROI_T2", HoldOutInstance=holdout_instance)
+    
     dataaug_instance.apply_augmentations(augmentation_types=["brightness", "gamma"], num_patients=75)
     holdout_instance.organizeAugmentation()
-    holdout_instance.holdout()
-    print(dataaug_instance.valid_roi_slices)
+
+    print()
+
     results_path = "/home/mariopasc/Python/Projects/BSC_final/epilepsy-displasia-focal-segmentation/images"
     data_explore.analyze_dataset("/home/mariopasc/Python/Datasets/T2FLAIR-ds-epilepsy", results_path)
 
